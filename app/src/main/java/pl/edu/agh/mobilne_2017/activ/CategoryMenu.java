62016567370f93a52564d6a93aee8e61a13536a4
Michal W <michalwilk93@hotmail.com>
2017-04-08 11:43:51 +0200

    Merge branch 'master' of https://github.com/mobilnelato/wspomaganie
    Conflicts:
    	.gitignore

---
+++ app/src/main/java/pl/edu/agh/mobilne_2017/activ/CategoryMenu.java
@@ -0,0 +1,16 @@
+package pl.edu.agh.mobilne_2017.activ;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import pl.edu.agh.mobilne_2017.R;
+
+public class CategoryMenu extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_category_layout);
+
+
+    }
+}
---
+++ app/src/main/java/pl/edu/agh/mobilne_2017/activ/NewCategory.java
@@ -0,0 +1,32 @@
+package pl.edu.agh.mobilne_2017.activ;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import pl.edu.agh.mobilne_2017.DatabaseHelper;
+import pl.edu.agh.mobilne_2017.R;
+
+
+public class NewCategory extends Activity {
+    private DatabaseHelper db;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_category_layout);
+        findViewById(R.id.savecat).setOnClickListener(new NewCategoryListener());
+    }
+
+    private class NewCategoryListener implements View.OnClickListener {
+        public void onClick(View arg0) {
+            db = new DatabaseHelper(getApplicationContext());
+            EditText tv = (EditText) findViewById(R.id.newcattitle);
+            Log.w("NewCategoryListener", tv.getText().toString());
+            db.createCategory(tv.getText().toString());
+        }
+
+    }
+}
